1fc466e3bea118b6730e134a8a84acbae392fb39
Emil Sirbu <sirbu_emil@yahoo.com>
2021-05-25 22:31:03 +0300

    add about-us and contact-us in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
 
   {
     path: '',
+    redirectTo: 'home', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomepageComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path: '**',
     component: HomepageComponent
   }
+
 ];
 
 @NgModule({
