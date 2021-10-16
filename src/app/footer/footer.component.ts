277e23bb232c3ed63cb4f5f412f5190ffc6bc660
Emil Sirbu <sirbu_emil@yahoo.com>
2021-10-16 15:54:16 +0300

    Add validation for newsletter form

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
+  form!: FormGroup;
+  email!: AbstractControl;
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
-  ngOnInit(): void {
+  constructor(fb: FormBuilder) {
+      this.form = fb.group({
+        'email': ['', [
+          Validators.pattern(this.emailPattern)
+        ]]
+      }); 
+
+      this.email = this.form.controls['email'];
+   }
+
+  submit(): void {
+    if (this.form.valid) {
+      this.form.reset()
+    } 
   }
-
 }
